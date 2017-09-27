7089750882bc4bb64a437eae6b70ad1da9331844
dmaupin <dm@davidmaupin.com>
2017-09-27 08:31:30 -0700

    a temporary landing page

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,3 +1,15 @@
+// tooltip
+
+var tooltipSpan = document.getElementById('tooltip');
+
+window.onmousemove = function (e) {
+    var x = e.clientX,
+        y = e.clientY;
+    tooltipSpan.style.top = (y + 20) + 'px';
+    tooltipSpan.style.left = (x + 20) + 'px';
+};
+
+
 // nav links - scroll to:
 
 $(document).ready(function() {
